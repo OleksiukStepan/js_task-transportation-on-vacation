d8a4f3f9ae84bfaed7553b51d05f2cbc3d112585
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-02-27 18:15:03 +0200

    implement solution for the task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,26 @@
+'use_strict';
+
+const RENT_COST = 40;
+const DISCOUNT_3_DAYS = 20;
+const DISCOUNT_7_DAYS = 50;
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const totalCost = RENT_COST * days;
+
+  if (days >= 7) {
+    return totalCost - DISCOUNT_7_DAYS;
+  }
+
+  if (days >= 3) {
+    return totalCost - DISCOUNT_3_DAYS;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
